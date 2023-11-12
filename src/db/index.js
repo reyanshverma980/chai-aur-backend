e15778bb312dc1c81ff693ef1c95c6364e541b79
Reyansh <ronitverma0202@gmail.com>
2023-11-13 04:01:49 +0530

    conected to mongodb atlas

---
+++ src/db/index.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectToDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI.replace(
+        "<password>",
+        process.env.MONGODB_PASSWORD
+      )}/${DB_NAME}`
+    );
+
+    console.log(
+      `Monogdb connected! DB host: ${connectionInstance.connection.host}`
+    );
+  } catch (error) {
+    console.log("Mongodb connection failed: ", error);
+    process.exit(1);
+  }
+};
+
+export default connectToDB;
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import "dotenv/config";
+import connectToDB from "./db/index.js";
+
+connectToDB();
