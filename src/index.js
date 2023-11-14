52d69445e1f8e611cdcee4c2b8ebcd0c89f066de
Reyansh <ronitverma0202@gmail.com>
2023-11-15 02:37:27 +0530

    added ApiError and ApiResponse files

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
 import "dotenv/config";
 import connectToDB from "./db/index.js";
+import { app } from "./app.js";
 
-connectToDB();
+connectToDB()
+  .then(() => {
+    app.on("error", (err) => {
+      console.log("Error: ", err);
+      throw err;
+    });
+		
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`app listening on port: ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Mongodb connection failed!", err);
+  });
